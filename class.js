1802161dd7f9118d6bede3e388fae8fc97caa641
fourseason7 <fourseason7@gmail.com>
2021-02-07 16:15:17 -0600

    add new java script study 1. class 2. object

---
+++ class.js
@@ -0,0 +1,102 @@
+class User{
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    get age(){
+        return this._age;
+    }
+
+    set age(value){
+        // this._age = value;
+        this._age = value < 0 ? 0: value;
+    }
+}
+
+const user1 = new User('Steve', 'Jobs', -1);
+console.log(user1.age);
+
+class Experiment {
+    publicField = 2;
+    #privateField = 0;
+}
+
+const experiment = new Experiment();
+console.log(experiment.publicField);
+console.log(experiment.privateField);
+
+class Article{
+    static publisher = 'Dream Coding';
+    constructor(articleNumber){
+        this.articleNumber = articleNumber;
+    }
+
+    static printPublisher(){
+        console.log(Article.publisher);
+    }
+}
+
+const article1 = new Article(1);
+const article2 = new Article(2);
+
+console.log(Article.publisher);
+Article.printPublisher();
+
+
+class Shape{
+    constructor(width, height, color){
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw(){
+        console.log(`drawing ${this.color} color of! `);
+    }
+    
+    getArea(){        
+        return this.width * this.height;
+    }
+}
+
+class Rectangle extends Shape{}
+class Triangle extends Shape {
+
+    getArea(){
+        return this.width * this.height / 2;
+    }
+
+    toString(){
+        return `Triangle: color : ${this.color}`
+    }
+}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw();
+console.log(rectangle.getArea());
+
+const triangle = new Triangle(20, 20, 'red');
+triangle.draw();
+console.log(triangle.getArea());
+
+
+console.log(rectangle instanceof Rectangle);
+console.log(triangle instanceof Rectangle);
+console.log(triangle instanceof Triangle);
+console.log(triangle instanceof Shape);
+console.log(triangle instanceof Object);
+
+console.log(triangle.toString());
+
+
+
+class Polycon{
+    constructor(){
+        this.name = 'Polygon';
+    }
+}
+
+const poly1 = new Polycon();
+console.log(poly1.name);
---
+++ object.js
@@ -0,0 +1,36 @@
+// 7.Fun cloning
+// Object.assign(dest, [obj1, obj2, obj3...])
+
+const user = {name: 'john', age: '20'};
+
+const user2 = user;
+
+// user2.name = 'coder';
+
+console.log(user2.name);
+console.log(user.name);
+
+
+// old way
+const user3 = {};
+for (key in user){
+    user3[key]= user[key];
+
+}
+console.clear();
+console.log(user3);
+
+const user4 = {};
+Object.assign(user4, user);
+console.log(user4);
+
+
+const user5 = Object.assign({}, user);
+console.log(user5);
+
+// another example
+const fruit1 = {color: 'red'};
+const fruit2 = {color: 'blue', size: 'big'};
+const mixed = Object.assign({}, fruit1, fruit2);
+console.log(mixed.color);
+console.log(mixed.size);
